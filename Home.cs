f14d3dcba31f5ceda4f20835c301211f8e957612
Sebastian Brummer <sebastianwabrummer@gmail.com>
2026-08-11 19:30:48 +0200

    Test change

--- Home.cs
+++ Home.cs
@@ -7,6 +7,7 @@ using System.Windows.Forms;
 using System.Xml.Linq;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
+//Testing
 namespace Byte_me___Group_2
 {
     // Home page: dashboard visuals (welcome message, search, theme toggle) + playlist functionality.
