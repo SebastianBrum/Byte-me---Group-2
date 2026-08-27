c0abaf54f69621dbb4b01d5ecac5201a3b2c2168
Sebastian Brummer <sebastianwabrummer@gmail.com>
2026-08-27 21:45:22 +0200

    Moved code out of Home.cs. AddNewSong, AddNewPLaylist.

---
+++ AddNewPlaylist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byte_me___Group_2
+{
+    internal class AddNewPlaylist
+    {
+    }
+}
