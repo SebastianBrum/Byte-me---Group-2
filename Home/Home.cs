d3b4aa6ca3c7e05c7d285cd9987c4287160e3726
Sebastian Brummer <sebastianwabrummer@gmail.com>
2026-09-07 18:33:18 +0200

    Random work for showcasing

--- Home/Home.cs
+++ Home/Home.cs
@@ -13,6 +13,8 @@ namespace Byte_me___Group_2
 {
     // Home page: dashboard visuals (welcome message, search, theme toggle) + playlist functionality.
     // Data is stored as plain text files under a "Data" folder next to the .exe.
+
+    // IDK random work
     public partial class Home : Form
     {
         private string username;
