5980e1eae7ecf18d143e67051028c9429c1f7c0e
Sebastian Brummer <sebastianwabrummer@gmail.com>
2026-09-08 18:44:03 +0200

    Added a song class

---
+++ Song/Song.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byte_me___Group_2
+{
+    public class Song
+    {
+        private string mName;
+        private string mArtist;
+        private string mDuration;
+
+        public string Name
+        {
+            get { return mName;  }
+            set { mName = value;  }
+        }
+
+        public string Artist
+        {
+            get { return mArtist; }
+            set { mArtist = value;}
+        }
+
+        public string duration
+        {
+            get { return mDuration}
+            set { mDuration = value; }
+        }
+    }
+}
